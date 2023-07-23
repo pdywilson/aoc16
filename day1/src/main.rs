0139dce77dad1a1003c03a203240eaf3364c390d
pdywilson <pdywilson@gmail.com>
2023-07-23 12:35:46 +0100

    day1 2016

---
+++ day1/src/main.rs
@@ -0,0 +1,88 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    println!("Hello, world!");
+    let file_path = "input.txt";
+
+    let mut file = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+            return;
+        }
+    };
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => {
+            println!("File content:\n{}", contents);
+        }
+        Err(e) => {
+            eprintln!("Error reading file: {}", e);
+        }
+    }
+
+    let cleaned_contents: String = contents.chars().filter(|c| !c.is_whitespace()).filter(|c| *c != '\n').collect();
+
+    let directions: Vec<&str> = cleaned_contents.split(',').collect();
+
+    println!("{:?}", directions);
+    
+
+    let directions_map: Vec<(char, i32)> = directions
+        .iter()
+        .map(|&direction| {
+            let first_char = direction.chars().next().unwrap();
+            let rest_string: String = direction.chars().skip(1).collect();
+            let rest = match rest_string.parse::<i32>() {
+                Ok(result) => result,
+                Err(e) => {
+                    eprintln!("Error parsing string: {}", e);
+                    // You can set a default value or handle the error in some other way here
+                    return (first_char, 0);  
+                }
+            };
+            (first_char, rest)
+        })
+        .collect();
+
+    println!("{:#?}", directions_map);
+
+
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut dir = 0; // 0 == N, 1 == E, 2 == S, 3 == W
+    let mut res: Vec<(i32, i32)> = Vec::new(); 
+    let mut done: bool = false;
+
+    for (direction, value) in &directions_map {
+        if (*direction == 'R') {
+            dir = (dir + 1) % 4;
+        }
+        if (*direction == 'L') {
+            dir = (dir + 3) % 4;
+        }
+        for i in 1..=*value {
+            if (dir == 0) {
+                y = y + 1;
+            }
+            if (dir == 1) {
+                x = x + 1;
+            }
+            if (dir == 2) {
+                y = y - 1;
+            }
+            if (dir == 3) {
+                x = x - 1;
+            }
+            if (!done && res.contains(&(x, y))) {
+                println!("result part 2: {}", x + y);
+                done = true;
+            }
+            res.push((x, y));
+        }
+    }
+    println!("result part 1: {}", x + y);
+
+}
